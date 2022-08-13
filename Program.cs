39ff368e37883fcf27de8f6dc6fbf88142ee835e
OlgaKarp7 <110402914+OlgaKarp7@users.noreply.github.com>
2022-08-13 11:38:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 5;
+int b = 7;
+int a2 = 2;
+int b2 = 10;
+int a3 = -9;
+int b3 = -3;
+
+int max = a;
+int max2 = a2;
+int max3 = a3;
+
+if ( a > max ) max = a;
+if ( b > max ) max = b;
+if ( a2 > max2 ) max2 = a2;
+if ( b2 > max2 ) max2 = b2;
+if ( a3 > max3 ) max3 = a3;
+if ( b3 > max3 ) max3 = b3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("max2 = ");
+Console.WriteLine(max2);
+
+Console.Write("max3 = ");
+Console.WriteLine(max3);
